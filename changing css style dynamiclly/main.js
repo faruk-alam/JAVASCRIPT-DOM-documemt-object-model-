386df2de6ae74921993f8cf8af96a11823a69dca
FARUK ALAM HAWLADER <59347740+faruk-alam@users.noreply.github.com>
2022-01-04 21:42:29 +0100

    Add files via upload

---
+++ changing css style dynamiclly/main.js
@@ -0,0 +1,9 @@
+
+function addStyle(){
+   var myPara = document.getElementById('para');
+   myPara.classList.add("para-style");
+}
+function removeStyle(){
+   var myPara = document.getElementById('para');
+   myPara.classList.remove("para-style");
+}
